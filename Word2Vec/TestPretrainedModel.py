e2f5feb985255ba0ad94411267b8bc60391233b1
ninetyfivejae <jh33836782@gmail.com>
2018-01-03 22:44:23 +0900

    most similar to given word simple method added 주어진 단어와 입력한 단어를 비교해서 가장 근접한 값을 반환하는 메소드 추가

--- Word2Vec/TestPretrainedModel.py
+++ Word2Vec/TestPretrainedModel.py
@@ -1,4 +1,4 @@
-import gensim
+'''import gensim
 
 # Load Google's pre-trained Word2Vec model.
 model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)  
@@ -15,3 +15,16 @@ print(model.doesnt_match("breakfast cereal dinner lunch".split()))
 
 #printing similarity index
 print(model.similarity('woman', 'man'))
+'''
+
+import gensim
+
+# Load Google's pre-trained Word2Vec model.
+model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)  
+#the model is loaded. It can be used to perform all of the tasks mentioned above.
+
+word = raw_input("Input word : ")
+while(word != "exit") :
+    print(model.most_similar_to_given(word, ['fire', 'watersupply', 'woman', 'teenager', 'gas', 'suicide', 'elder', 'child', 'disaster', 'rescue']))
+    print(model.most_similar(positive=['grandfather', 'cancer', 'emergency']))
+    word = raw_input("Input word : ")
