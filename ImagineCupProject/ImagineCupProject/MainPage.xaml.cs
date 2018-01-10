6b9f6d91237658c6ea89facc83e9226c5b7ed8a7
DESKTOP-LESU3Q0\jae <jh33836782@gmail.com>
2018-01-11 05:55:25 +0900

    Async Task method memory error fixed with OS memory management environment settings 윈도우 메모리 환경 설정을 가상메모리 사용으로 바꿔서 비동기 작업 메모리 문제 해결 했음

--- ImagineCupProject/ImagineCupProject/MainPage.xaml.cs
+++ ImagineCupProject/ImagineCupProject/MainPage.xaml.cs
@@ -284,51 +284,7 @@ namespace ImagineCupProject
         {
 
         }
-
-        //private void word2vec_Click(object sender, RoutedEventArgs e)
-        //{
-        //    try
-        //    {
-        //        //다른 파이썬으로 실행
-        //        string python = @"C:\Python36\python.exe";
-        //        //string python = Environment.CurrentDirectory + @"\Python36\python.exe";
-        //        string myPythonApp = "WordClassification.py";
-
-        //        //string으로 핵심단어들 받아서, List에 넣고, List에서 단어 하나씩 뽑아서 하나의 string으로 만듬
-        //        //string wordOne, wordTwo, keyWords = "";
-        //        //List<string> exampleList = new List<string>();
-        //        //exampleList.Add("ONE");
-        //        //exampleList.Add("TWO");
-        //        //exampleList.Add("THREE");
-        //        //foreach (string i in exampleList)
-        //        //{
-        //        //    keyWords = keyWords + i + ",";
-        //        //}
-        //        //keyWords = keyWords.Remove(keyWords.Length - 1);
-
-        //        string keyWords = problemText.Text.Remove(problemText.Text.Length - 1);
-
-        //        ProcessStartInfo myProcessStartInfo = new ProcessStartInfo(python);
-        //        myProcessStartInfo.UseShellExecute = false;
-        //        myProcessStartInfo.RedirectStandardOutput = true;
-        //        myProcessStartInfo.Arguments = myPythonApp + " " + keyWords;
-
-        //        Process myProcess = new Process();
-        //        myProcess.StartInfo = myProcessStartInfo;
-        //        myProcess.Start();
-        //        StreamReader myStreamReader = myProcess.StandardOutput;
-        //        string codeResult = myStreamReader.ReadToEnd();
-        //        myProcess.WaitForExit();
-        //        myProcess.Close();
-
-        //        codeText.Text = codeResult;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        codeText.Text = ex.Message;
-        //    }
-        //}
-
+        
         private void word2vec_Click(object sender, RoutedEventArgs e)
         {
             Run(problemText.Text);
